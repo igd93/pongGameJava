368f44ff1fab7200aec498a139b6ce9352639ab5
igd93 <102153262+igd93@users.noreply.github.com>
2023-04-01 22:44:19 +0200

    Add files via upload

---
+++ src/Ball.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.util.*;
+
+public class Ball extends Rectangle {
+
+    Random random;
+    int xVelocity;
+    int yVelocity;
+    int initial_speed = 2;
+
+    Ball(int x, int y, int width, int height){
+
+        super(x, y, width, height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if (randomXDirection == 0) {
+            randomXDirection--;
+        }
+        setXDirection(randomXDirection*initial_speed);
+
+        int randomYDirection = random.nextInt(2);
+        if (randomYDirection == 0) {
+            randomYDirection--;
+        }
+        setYDirection(randomYDirection * initial_speed);
+
+
+    }
+
+    public void setXDirection(int randomXDirection) {
+
+        xVelocity = randomXDirection;
+
+    }
+
+    public void setYDirection(int randomYDirection) {
+
+        yVelocity = randomYDirection;
+
+    }
+
+    public void move() {
+        
+        x += xVelocity;
+        y += yVelocity;
+    }
+
+    public void draw(Graphics g) {
+
+        g.setColor(Color.white);
+        g.fillOval(x, y, width, height);
+
+    }
+    
+}
